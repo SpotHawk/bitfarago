2fe8fd00944b99445cc6324317038c62c3f3b5b7
Patrik Németh <npatrik03@gmail.com>
2021-10-26 22:40:04 +0200

    3.fordulo, reszponzivitas,szoveg es csik anim

--- 3.forduló/main.js
+++ 3.forduló/main.js
@@ -1,9 +1,13 @@
 let leiras = document.querySelector("#leiras");
+let vonal=document.querySelector("#vonal");
 let menu = document.querySelector("#menu");
 let jatekmezo = document.querySelector("#jatekmezo");
 let jatekmenu = document.querySelector("#jatekmenu");
 jatekmezo.style.visibility = "hidden";
 jatekmenu.style.visibility = "hidden";
+vonal.style.width="40%";
+vonal.style.marginLeft="auto";
+vonal.style.marginRight="auto";
 function Konnyu() {
     menu.style.visibility = "hidden";
     jatekmezo.style.visibility = "visible";
@@ -31,29 +35,67 @@ function Legendas() {
 }
 
 function Torol() {
-    leiras.innerHTML="";
+    vonal.style.transform="scale(0)";
+    vonal.borderBottom="1px solid white";
+    vonal.style.transition="transform 500ms ease-in-out";
+    AnimacioKi();
 }
 function KonnyuLeiras() {
     leiras.innerHTML = 
-    "Kódok megjelenése: mp-ként 2 <br/> Kódok eltűnése: 10 mp-ként <br/> Keresendő kódok: játék közben megjelenítve <br/> Segítségek: időlassítás,  időfagyasztás, hitstreak, jók megjelenítése <br/> Rossz kód: pontlevonás";
+    "<p>Kódok megjelenése: mp-ként 2 <br/> Kódok eltűnése: 10 mp-ként <br/> Keresendő kódok: játék közben megjelenítve <br/> Segítségek: időlassítás,  időfagyasztás, hitstreak, jók megjelenítése <br/> Rossz kód: pontlevonás</p>";
+    Animacio();
+    VonalAnimacio();
 }
 function KozepesLeiras() {
     leiras.innerHTML = 
-    "Kódok megjelenése: mp-ként 1 <br/> Kódok eltűnése: 10 mp-ként <br/> Keresendő kódok: játék közben megjelenítve <br/> Segítségek: időlassítás,  időfagyasztás, hitstreak, jók megjelenítése <br/> Rossz kód: pontlevonás";
+    "<p>Kódok megjelenése: mp-ként 1 <br/> Kódok eltűnése: 10 mp-ként <br/> Keresendő kódok: játék közben megjelenítve <br/> Segítségek: időlassítás,  időfagyasztás, hitstreak, jók megjelenítése <br/> Rossz kód: pontlevonás</p>";
+    Animacio();
+    VonalAnimacio();
 }
 function NehezLeiras() {
     leiras.innerHTML = 
-    "Kódok megjlenése: mp-ként 1 <br/> Kódok eltűnése: 5 mp-ként <br/> Keresendő kódok: játék előtt megjelenítve <br/> Segítségek: időlassítás,  időfagyasztás, hitstreak <br/> Rossz kód: folyamatos pontlevonás, 5 rossz kód után game over";
+    "<p>Kódok megjlenése: mp-ként 1 <br/> Kódok eltűnése: 5 mp-ként <br/> Keresendő kódok: játék előtt megjelenítve <br/> Segítségek: időlassítás,  időfagyasztás, hitstreak <br/> Rossz kód: folyamatos pontlevonás, 5 rossz kód után game over</p>";
+    Animacio();
+    VonalAnimacio();
 }
 function NagyonNehezLeiras() {
     leiras.innerHTML = 
-    "Kódok megjlenése: mp-ként 1 <br/> Kódok eltűnése: 5 mp-ként <br/> Keresendő kódok: játék előtt megjelenítve <br/> Segítségek: időlassítás, hitstreak <br/> Rossz kód: folyamatos pontlevonás, 3 rossz kód után game over";
+    "<p>Kódok megjlenése: mp-ként 1 <br/> Kódok eltűnése: 5 mp-ként <br/> Keresendő kódok: játék előtt megjelenítve <br/> Segítségek: időlassítás, hitstreak <br/> Rossz kód: folyamatos pontlevonás, 3 rossz kód után game over</p>";
+    Animacio();
+    VonalAnimacio();
 }
 function LegendasLeiras() {
     leiras.innerHTML = 
-    "Kódok megjelenése: mp-ként 1 <br/> Kódok eltűnése: 5 mp-ként <br/> Keresendő kódok: játék előtt megjelenítve <br/> Segítségek: hitstreak <br/> Rossz kód: Egyetlen egy rossz kód után game over";
+    "<p>Kódok megjelenése: mp-ként 1 <br/> Kódok eltűnése: 5 mp-ként <br/> Keresendő kódok: játék előtt megjelenítve <br/> Segítségek: hitstreak <br/> Rossz kód: Egyetlen egy rossz kód után game over</p>";
+    Animacio();
+    VonalAnimacio();
 }
 
+function Animacio()
+{
+    leiras.classList.remove("slide-out"); 
+    void leiras.offsetWidth; 
+    leiras.classList.remove("slide-left"); 
+    void leiras.offsetWidth; 
+    leiras.classList.add("slide-left");
+}
+
+function AnimacioKi()
+{
+    leiras.classList.remove("slide-left"); 
+    void leiras.offsetWidth;
+    leiras.classList.remove("slide-out"); 
+    void leiras.offsetWidth; 
+    leiras.classList.add("slide-out");
+    setTimeout(()=>{leiras.innerHTML="";},490);
+}
+
+function VonalAnimacio()
+{
+    //void vonal.offsetWidth;
+    vonal.style.transform="scale(1)";
+    vonal.style.borderBottom="1px solid white";
+}
 
 function Uj_Jatek() {
     menu.style.visibility = "visible";
